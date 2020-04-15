2abb8c693ee3564467c574ba43b08d0f6cea77b7
monibu1548 <monibu1548@gmail.com>
2020-04-15 13:07:34 +0900

    Initial commit

---
+++ Example/JKExtension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  JKExtension
+//
+//  Created by monibu1548 on 04/15/2020.
+//  Copyright (c) 2020 monibu1548. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+

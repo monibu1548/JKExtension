0fa85aeb71e7957d61590e60b5b0401ba2020666
monibu1548 <monibu1548@gmail.com>
2020-06-27 19:42:26 +0900

    Add Collection extension func

---
+++ JKExtension/Classes/Collection+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Collection+Extension.swift
+//  JKExtension
+//
+//  Created by 정진규 on 2020/06/27.
+//
+
+import Foundation
+
+extension Collection {
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}

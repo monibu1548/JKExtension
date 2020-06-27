a15145d3c28146428964d90244ae829f1f746800
monibu1548 <monibu1548@gmail.com>
2020-06-27 19:45:00 +0900

    Update collection extension func access control

--- JKExtension/Classes/Collection+Extension.swift
+++ JKExtension/Classes/Collection+Extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Collection {
-    subscript (safe index: Index) -> Element? {
+    public subscript (safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
 }
